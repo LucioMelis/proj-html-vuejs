5add0279bf05fe5b447c69a9a4a8da75fc95b127
luciomelis <luciomelis1992@gmail.com>
2022-04-26 16:43:28 +0200

    ho importato le icone che mi occorrono

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+// Importazione FontAwesomeIcon 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import { faGem } from '@fortawesome/free-regular-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faTemperatureLow } from '@fortawesome/free-solid-svg-icons'
+import { faBoxesStacked } from '@fortawesome/free-solid-svg-icons'
+import { faTruckRampBox } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+library.add(faClock, faPhone, faEnvelope, faFacebookF, faLocationDot, faQuoteRight, faGem, faTwitter, faLinkedinIn, faUser, faTemperatureLow, faTruckRampBox, faBoxesStacked)
 
 Vue.config.productionTip = false
 
