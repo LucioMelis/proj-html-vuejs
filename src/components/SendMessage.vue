c2acae4e8508201180eebb64a17a48614655ce20
luciomelis <luciomelis1992@gmail.com>
2022-04-27 21:23:05 +0200

    Creato nuovo Componente

---
+++ src/components/SendMessage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Ciao Input</div>
+  <!-- collegare al main il componente  -->
+</template>
+
+<script>
+export default {
+  name: "SendMessage",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/variabili.scss";
+</style>
